ed2c1d063c1ac688edea8c9d814205ce56f89ff6
Welber Nogueira <welberk87@gmail.com>
2024-08-27 18:55:44 -0300

    Primeiro commit: projeto inicializado

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const user = "Welber";
+  res.send(
+    `Realizei de forma completa atividade solicitada... Acesso permitido para ${user}.`
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
